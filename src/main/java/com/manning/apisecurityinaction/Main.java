8302f59f17fd2c744533c63ed262b8cab490e5c4
Francisco Filho <francisco@capimgrosso.com>
2022-09-28 21:04:52 -0300

    capitulo2 completo

---
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -0,0 +1,53 @@
+package com.manning.apisecurityinaction;
+
+import com.manning.apisecurityinaction.controller.SpaceController;
+import org.dalesbred.Database;
+import org.h2.jdbcx.JdbcConnectionPool;
+import org.json.JSONObject;
+import spark.Request;
+import spark.Response;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        var datasource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter_api_user", "password");
+        var database = Database.forDataSource(datasource);
+
+        createTables(database);
+
+        var spaceController = new SpaceController(database);
+
+        post("/spaces", spaceController::createSpace);
+
+        afterAfter((req, res) -> {
+            res.type("application/json");
+            res.header("Server", "");
+            res.header("X-XSS-Protection", "0");
+        });
+
+        notFound(new JSONObject().put("notfound", "just a 404").toString());
+        exception(IllegalArgumentException.class, Main::badRequest);
+        internalServerError(new JSONObject().put("error", "internal server error").toString());
+    }
+
+    private static void badRequest(Exception ex, Request request, Response response) {
+        response.status(400);
+        response.body(new JSONObject().put("error", ex.getMessage()).toString());
+    }
+
+    private static void createTables(Database database) {
+        try {
+            Path schemaPath = Paths.get(Main.class.getResource("/schema.sql").toURI());
+            database.update(Files.readString(schemaPath));
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
@@ -0,0 +1,39 @@
+package com.manning.apisecurityinaction.controller;
+
+import org.dalesbred.Database;
+import org.json.JSONObject;
+import spark.Request;
+import spark.Response;
+
+public class SpaceController {
+    private final Database database;
+
+    public SpaceController(Database database) {
+        this.database = database;
+    }
+
+    public JSONObject createSpace(Request req, Response res){
+        var body = new JSONObject(req.body());
+        var spaceName = body.getString("name");
+        if (spaceName.length() > 255){
+            throw new IllegalArgumentException("The length of the name should be less than 255 characters");
+        }
+        var owner = body.getString("owner");
+        if (!owner.matches("[a-zA-Z][a-zA-Z0-9]{1,29}")){
+            throw new IllegalArgumentException("The owner name should have only letters and numbers");
+        }
+
+        return database.withTransaction(tx -> {
+            var spaceId = database.findUniqueLong("SELECT NEXT VALUE FOR space_id_seq");
+
+            database.updateUnique("INSERT INTO spaces (space_id, name, owner) VALUES (?, ?, ?)",
+                    spaceId, spaceName, owner);
+
+            res.status(201);
+            res.header("Location", "/spaces/" + spaceId);
+            return new JSONObject()
+                    .put("name", spaceName)
+                    .put("uri", "/spaces/" + spaceId );
+        });
+    }
+}
